0a0288d6269c402f05dd24346ba431010193d5bf
gagil <german.a.gil@gmail.com>
2017-07-31 09:49:11 -0600

    detached criteria
    Sea agrega detached criteria a la implementacion del finder

--- src/main/java/org/gagl/fwk/dao/DaoFinder.java
+++ src/main/java/org/gagl/fwk/dao/DaoFinder.java
@@ -1,7 +1,10 @@
 package org.gagl.fwk.dao;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+
+import org.hibernate.criterion.DetachedCriteria;
 
 public interface DaoFinder {
 	
@@ -9,6 +12,8 @@ public interface DaoFinder {
 	public <T> List<T> findByQueryName(String pSqlQuery, Map<Integer,Object> pParams);
 	public <T> List<T> findByQueryName(String pSqlQuery, Map<Integer,Object> pParams,@SuppressWarnings("rawtypes") Class pEntity);
 	public <T> List<T> findBySqlQuery(String pSqlQuery, @SuppressWarnings("rawtypes") Class pEntity);
-	public <T> List<T> findBySqlQuery(String pSqlQuery, Map<Integer,Object> pParams,@SuppressWarnings("rawtypes") Class pEntity);		
+	public <T> List<T> findBySqlQuery(String pSqlQuery, Map<Integer,Object> pParams,@SuppressWarnings("rawtypes") Class pEntity);
+	public <T> List<T> findByDetachedCriteria(DetachedCriteria pCriteria);
+	public <T> T uniqueResult(Collection<T> pList);	
 
 }
